b39b87047b259b93a5d2bdd7345ccf239dd43f6b
Uzo Olisemeka <uzo@designbymobius.ca>
2020-08-29 21:29:32 -0400

    convert entry point program to 'task' format

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log( 'hello world' );
+var create_task = require('cjs-task'),
+    app = create_task();
+
+app.callback( function( error ){
+  var log_entry = [
+    'action=exit',
+    'reason="'+ ( error ? 'error' : 'app ended' ) + '"'
+  ];
+
+  if( error ){
+    log_entry.push(
+      'error="'+ error.message + '"',
+      'stack=\n---\n'+ error.stack +'\n---'
+    );
+  }
+
+  console.log( log_entry.join(' ') );
+  process.exit( error ? 1 : 0 );
+});
+
+app.step( 'echo hello world', function(){
+  console.log( 'hello world' );
+  app.next();
+});
+
+app.start();
