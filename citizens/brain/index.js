6d541534036494871017c1015313ee804283a7ac
Uzo Olisemeka <uzo@designbymobius.ca>
2020-09-01 08:37:09 -0400

    minor code cleanup

--- citizens/brain/index.js
+++ citizens/brain/index.js
@@ -1,4 +1,5 @@
 var create_task = require('cjs-task'),
+    path_to_root = '../..',
     app = create_task();
 
 app.callback( function( error ){
@@ -18,76 +19,69 @@ app.step( 'if an error isnt caught, end app', function(){
   app.next();
 });
 
-require( '../../steps/supe/create-citizen' )( app );
+require( path_to_root + '/steps/supe/create-citizen' )( app );
+require( path_to_root + '/steps/browser/start' )( app, { headless: false });
+require( path_to_root + '/steps/browser/load-page' )( app, { url: 'https://registration.myoet.com/login.jsp' });
 
-app.step( 'setup app', function(){
-  var path_to_root = app.get( 'path-to-root' );
+app.step( 'login to oet account', function(){
+  var page = app.get( 'browser-page' ),
+      user = process.env.OET_USERNAME,
+      pass = process.env.OET_PASSWORD;
 
-  require( path_to_root + '/steps/browser/start' )( app, { headless: false });
-  require( path_to_root + '/steps/browser/load-page' )( app, { url: 'https://registration.myoet.com/login.jsp' });
+  if( ! user ) throw new Error( 'OET username not specified' );
+  if( ! pass ) throw new Error( 'OET password not specified' );
 
-  app.step( 'login to oet account', function(){
-    var page = app.get( 'browser-page' ),
-        user = process.env.OET_USERNAME,
-        pass = process.env.OET_PASSWORD;
+  console.log( 'action=log-credentials user='+ user + ' pass='+ pass );
 
-    if( ! user ) throw new Error( 'OET username not specified' );
-    if( ! pass ) throw new Error( 'OET password not specified' );
+  var oet_credentials = {};
+      oet_credentials.user = user;
+      oet_credentials.pass = pass;
 
-    console.log( 'action=log-credentials user='+ user + ' pass='+ pass );
-
-    var oet_credentials = {};
-        oet_credentials.user = user;
-        oet_credentials.pass = pass;
-
-    page.evaluate( function( credentials ){
-      document.querySelector( '#u' ).value = credentials.user;
-      document.querySelector( '#p' ).value = credentials.pass;
-      document.querySelector( '#s' ).click();
-    }, oet_credentials )
-      .then( function(){
-        // do nothing. form submission should navigate to next task step
-      })
-      .catch( app.end );
-  });
-
-  app.step( 'wait til page hash changes; virtual routing', function(){
-    var page = app.get( 'browser-page' );
-
-    page.evaluate( function(){
-      return new Promise( function( resolve, reject ){
-        window.addEventListener( 'hashchange', resolve );
-      });
+  page.evaluate( function( credentials ){
+    document.querySelector( '#u' ).value = credentials.user;
+    document.querySelector( '#p' ).value = credentials.pass;
+    document.querySelector( '#s' ).click();
+  }, oet_credentials )
+    .then( function(){
+      // do nothing. form submission should navigate to next task step
     })
-      .then( app.next )
-      .catch( app.end );
-  });
+    .catch( app.end );
+});
 
-  app.step( 'log account details', function(){
-    var page = app.get( 'browser-page' );
+app.step( 'wait til page hash changes; virtual routing', function(){
+  var page = app.get( 'browser-page' );
 
-    page.evaluate( function(){
-      var details_dom = document.querySelector( '.tsc-memberdetails > .details' ),
-          mixed_details_dom = details_dom.querySelectorAll( 'p' ),
-          name_dom = mixed_details_dom[0],
-          exam_id_dom = mixed_details_dom[1],
-          img_dom = details_dom.querySelector( 'div > img' );
+  page.evaluate( function(){
+    return new Promise( function( resolve, reject ){
+      window.addEventListener( 'hashchange', resolve );
+    });
+  })
+    .then( app.next )
+    .catch( app.end );
+});
 
-      var details = {};
-          details.name = name_dom.innerText;
-          details.exam_id = exam_id_dom.innerText;
-          details.img = img_dom.src;
+app.step( 'log account details', function(){
+  var page = app.get( 'browser-page' );
 
-      return details;
+  page.evaluate( function(){
+    var details_dom = document.querySelector( '.tsc-memberdetails > .details' ),
+        mixed_details_dom = details_dom.querySelectorAll( 'p' ),
+        name_dom = mixed_details_dom[0],
+        exam_id_dom = mixed_details_dom[1],
+        img_dom = details_dom.querySelector( 'div > img' );
+
+    var details = {};
+        details.name = name_dom.innerText;
+        details.exam_id = exam_id_dom.innerText;
+        details.img = img_dom.src;
+
+    return details;
+  })
+    .then( function( details ){
+      console.log( 'action=log-oet-account-details exam-id='+ details.exam_id +' name="'+ details.name + '"' );
+      app.next();
     })
-      .then( function( details ){
-        console.log( 'action=log-oet-account-details exam-id='+ details.exam_id +' name="'+ details.name + '"' );
-        app.next();
-      })
-      .catch( app.end );
-  });
-
-  app.next();
+    .catch( app.end );
 });
 
 app.step( 'wait a bit, then exit', function(){
