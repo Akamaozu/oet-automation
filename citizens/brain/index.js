4308ec08afbc7541197e814bfab9a39f74e6fad2
Uzo Olisemeka <uzo@designbymobius.ca>
2020-09-01 08:25:39 -0400

    brain: log into oet account, get exam id and examinee's name

--- citizens/brain/index.js
+++ citizens/brain/index.js
@@ -31,20 +31,72 @@ app.step( 'setup app', function(){
         user = process.env.OET_USERNAME,
         pass = process.env.OET_PASSWORD;
 
+    if( ! user ) throw new Error( 'OET username not specified' );
+    if( ! pass ) throw new Error( 'OET password not specified' );
+
     console.log( 'action=log-credentials user='+ user + ' pass='+ pass );
 
-    page.evaluate( function( config ){
-      document.querySelector( '#u' ).value = config.user;
-      document.querySelector( '#p' ).value = config.pass;
+    var oet_credentials = {};
+        oet_credentials.user = user;
+        oet_credentials.pass = pass;
+
+    page.evaluate( function( credentials ){
+      document.querySelector( '#u' ).value = credentials.user;
+      document.querySelector( '#p' ).value = credentials.pass;
       document.querySelector( '#s' ).click();
-    }, { user: user, pass: pass })
+    }, oet_credentials )
+      .then( function(){
+        // do nothing. form submission should navigate to next task step
+      })
+      .catch( app.end );
+  });
+
+  app.step( 'wait til page hash changes; virtual routing', function(){
+    var page = app.get( 'browser-page' );
+
+    page.evaluate( function(){
+      return new Promise( function( resolve, reject ){
+        window.addEventListener( 'hashchange', resolve );
+      });
+    })
+      .then( app.next )
+      .catch( app.end );
+  });
+
+  app.step( 'log account details', function(){
+    var page = app.get( 'browser-page' );
+
+    page.evaluate( function(){
+      var details_dom = document.querySelector( '.tsc-memberdetails > .details' ),
+          mixed_details_dom = details_dom.querySelectorAll( 'p' ),
+          name_dom = mixed_details_dom[0],
+          exam_id_dom = mixed_details_dom[1],
+          img_dom = details_dom.querySelector( 'div > img' );
+
+      var details = {};
+          details.name = name_dom.innerText;
+          details.exam_id = exam_id_dom.innerText;
+          details.img = img_dom.src;
+
+      return details;
+    })
+      .then( function( details ){
+        console.log( 'action=log-oet-account-details exam-id='+ details.exam_id +' name="'+ details.name + '"' );
+        app.next();
+      })
+      .catch( app.end );
   });
 
   app.next();
 });
 
-app.step( 'wait', function(){
-  // do nothing
+app.step( 'wait a bit, then exit', function(){
+  var sec_in_ms = 1000,
+      delay_s = 10,
+      delay_ms = delay_s * sec_in_ms;
+
+  console.log( 'action=delay-exit duration='+ delay_s +'s' );
+  setTimeout( app.end, delay_ms );
 });
 
 app.start();
